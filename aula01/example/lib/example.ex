a71ff0fccc42171b03644ce326ddb62c1f3dda4f
ri-favaro <142619062+ri-favaro@users.noreply.github.com>
2026-03-19 15:00:36 -0300

    Usando o condição if

--- aula01/example/lib/example.ex
+++ aula01/example/lib/example.ex
@@ -1,13 +1,20 @@
 defmodule Example do
   use Application
-  alias UUID
+
 
   def start(_type, _args) do
-    IO.puts(UUID.uuid4())
+    Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
-  def hello do
-    :world
+  def main do
+    name = "Ricardo"
+    status = Enum.random([:gold, :silver, :bronze])
+
+    if status === :gold do
+      IO.puts("Bem vindo, #{name}")
+    else
+      IO.puts("Faça upgrade para gold")
+    end
   end
 end
